960cebf5f548e8cbbd49ce5f39a388cefb876368
Divine Higgins <dhiggins94@gmail.com>
2021-11-23 15:51:18 -0500

    added comments to cockraoch.js

--- beginner-series-4-cockroach.js
+++ beginner-series-4-cockroach.js
@@ -4,3 +4,15 @@ function cockroachSpeed(s) {
   console.log(speed)
   return speed
 }
+
+// refactor
+function cockroachSpeed(s) {
+  //Good Luck!
+  let speed = Math.floor((s * 1000 * 100) / ( 60 * 60))
+  return speed
+}
+
+// km = 1000 meters
+// m = 100 cm
+// seconds = 60 seconds
+// hours = 60 minutes
